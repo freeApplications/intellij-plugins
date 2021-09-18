358c0e2fa80d8dd2d23a3411a1b951ec9a4348c2
yutaka-sugao <yutaka.sugao+github@gmail.com>
2021-09-18 14:48:24 +0900

    :white_check_mark: Add tests for invalid types

--- php-array-converter/src/test/kotlin/jp/freeapps/intellij/plugin/phparray/converter/JsonConverterTest.kt
+++ php-array-converter/src/test/kotlin/jp/freeapps/intellij/plugin/phparray/converter/JsonConverterTest.kt
@@ -4,84 +4,145 @@ import com.intellij.json.psi.JsonFile
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import io.mockk.every
 import io.mockk.mockkObject
+import jp.freeapps.intellij.plugin.phparray.exception.ConvertException
 import jp.freeapps.intellij.plugin.phparray.settings.AppSettingsState
+import org.junit.Ignore
+import org.junit.Rule
 import org.junit.Test
+import org.junit.experimental.runners.Enclosed
+import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameter
 import org.junit.runners.Parameterized.Parameters
 import java.io.File
 
-@RunWith(Parameterized::class)
-internal class JsonConverterTest : LightJavaCodeInsightFixtureTestCase() {
-    @Parameter
-    lateinit var title: String
+@RunWith(Enclosed::class)
+internal class JsonConverterTest {
+    @RunWith(Parameterized::class)
+    internal class ToPhpArrayTest : LightJavaCodeInsightFixtureTestCase() {
+        @Parameter
+        lateinit var title: String
 
-    @Parameter(1)
-    lateinit var settings: AppSettingsState
+        @Parameter(1)
+        lateinit var settings: AppSettingsState
 
-    @Parameter(2)
-    lateinit var argument: String
+        @Parameter(2)
+        lateinit var argument: String
 
-    @Parameter(3)
-    lateinit var expected: String
+        @Parameter(3)
+        lateinit var expected: String
 
-    @Test
-    fun test() {
-        // setup:
-        mockkObject(AppSettingsState)
-        every { AppSettingsState.getInstance() } returns settings
-        val psiFile = myFixture.configureByText("target.json", argument)
-        val jsonConverter = JsonConverter(psiFile as JsonFile)
+        @Test
+        fun test() {
+            // setup:
+            mockkObject(AppSettingsState)
+            every { AppSettingsState.getInstance() } returns settings
+            val psiFile = myFixture.configureByText("target.json", argument)
+            val jsonConverter = JsonConverter(psiFile as JsonFile)
 
-        // when:
-        val actual = jsonConverter.toPhpArray()
+            // when:
+            val actual = jsonConverter.toPhpArray()
 
-        // then:
-        assertEquals(expected, actual)
+            // then:
+            assertEquals(expected, actual)
+        }
+
+        companion object {
+            @JvmStatic
+            @Parameters(name = "{0}")
+            fun dataProvider(): Iterable<Any> {
+                return listOf(
+                    createPattern("keepFormat", "useArray,useSingleQuote", false, false),
+                    createPattern("keepFormat", "useBraket,useSingleQuote", true, false),
+                    createPattern("keepFormat", "useArray,useDoubleQuote", false, true),
+                    createPattern("keepFormat", "useBraket,useDoubleQuote", true, true),
+                    createPattern("syntax", "useArray,useSingleQuote", false, false),
+                    createPattern("syntax", "useBraket,useSingleQuote", true, false),
+                    createPattern("syntax", "useArray,useDoubleQuote", false, true),
+                    createPattern("syntax", "useBraket,useDoubleQuote", true, true),
+                    createPattern("escapeSequence", "useArray,useSingleQuote", false, false),
+                    createPattern("escapeSequence", "useBraket,useSingleQuote", true, false),
+                    createPattern("escapeSequence", "useArray,useDoubleQuote", false, true),
+                    createPattern("escapeSequence", "useBraket,useDoubleQuote", true, true),
+                    createPattern("lastElementComma", "append", false, false, true),
+                    createPattern("lastElementComma", "remove", false, false, false),
+                )
+            }
+
+            private fun createPattern(
+                directory: String,
+                pattern: String,
+                useBraket: Boolean,
+                useDoubleQuote: Boolean,
+                appendComma: Boolean = false
+            ): Array<Any> {
+                val settings = AppSettingsState()
+                settings.useBraket = useBraket
+                settings.useDoubleQuote = useDoubleQuote
+                settings.appendComma = appendComma
+                return arrayOf(
+                    "$directory($pattern)",
+                    settings,
+                    loadResource(directory, pattern, false),
+                    loadResource(directory, pattern, true),
+                )
+            }
+        }
     }
 
+    @RunWith(Parameterized::class)
+    internal class CheckValidTypeTest : LightJavaCodeInsightFixtureTestCase() {
+        @Parameter
+        lateinit var title: String
+
+        @Parameter(1)
+        lateinit var argument: String
+
+        @Parameter(2)
+        lateinit var expected: String
+
+        @get:Rule
+        val thrown: ExpectedException? = ExpectedException.none()
+
+        @Test
+        fun test() {
+            // setup:
+            val psiFile = myFixture.configureByText("target.json", argument)
+            val jsonConverter = JsonConverter(psiFile as JsonFile)
+
+            // expect:
+            thrown!!.expect(ConvertException::class.java)
+            thrown!!.expectMessage(expected)
+            jsonConverter.toPhpArray()
+        }
+
+        companion object {
+            @JvmStatic
+            @Parameters(name = "{0}")
+            fun dataProvider(): Iterable<Any> {
+                return listOf(
+                    *createPatterns("syntaxError", 3),
+                    *createPatterns("literalTypeError", 2),
+                )
+            }
+
+            private const val directory = "invalidType"
+            private fun createPatterns(pattern: String, numberOfPatterns: Int): Array<Any> {
+                return Array(numberOfPatterns) {
+                    val number = it + 1
+                    arrayOf(
+                        "$directory($pattern#$number)",
+                        loadResource("$directory/$pattern", "#$number", false),
+                        loadResource("$directory/$pattern", "#$number", true),
+                    )
+                }
+            }
+        }
+    }
+
+    @Ignore
     companion object {
-        @JvmStatic
-        @Parameters(name = "{0}")
-        fun dataProvider(): Iterable<Any> {
-            return listOf(
-                createPattern("keepFormat", "useArray,useSingleQuote", false, false),
-                createPattern("keepFormat", "useBraket,useSingleQuote", true, false),
-                createPattern("keepFormat", "useArray,useDoubleQuote", false, true),
-                createPattern("keepFormat", "useBraket,useDoubleQuote", true, true),
-                createPattern("syntax", "useArray,useSingleQuote", false, false),
-                createPattern("syntax", "useBraket,useSingleQuote", true, false),
-                createPattern("syntax", "useArray,useDoubleQuote", false, true),
-                createPattern("syntax", "useBraket,useDoubleQuote", true, true),
-                createPattern("escapeSequence", "useArray,useSingleQuote", false, false),
-                createPattern("escapeSequence", "useBraket,useSingleQuote", true, false),
-                createPattern("escapeSequence", "useArray,useDoubleQuote", false, true),
-                createPattern("escapeSequence", "useBraket,useDoubleQuote", true, true),
-                createPattern("lastElementComma", "append", false, false, true),
-                createPattern("lastElementComma", "remove", false, false, false),
-            )
-        }
-
-        private fun createPattern(
-            directory: String,
-            pattern: String,
-            useBraket: Boolean,
-            useDoubleQuote: Boolean,
-            appendComma: Boolean = false
-        ): Array<Any> {
-            val settings = AppSettingsState()
-            settings.useBraket = useBraket
-            settings.useDoubleQuote = useDoubleQuote
-            settings.appendComma = appendComma
-            return arrayOf(
-                "$directory($pattern)",
-                settings,
-                loadResource(directory, pattern, false),
-                loadResource(directory, pattern, true),
-            )
-        }
-
         private const val root = "src/test/resources/jsonToPhpArray"
         private fun loadResource(directory: String, pattern: String, isExpected: Boolean): String {
             val name = if (isExpected) "expected" else "argument"
--- php-array-converter/src/test/kotlin/jp/freeapps/intellij/plugin/phparray/converter/PhpArrayConverterTest.kt
+++ php-array-converter/src/test/kotlin/jp/freeapps/intellij/plugin/phparray/converter/PhpArrayConverterTest.kt
@@ -2,67 +2,128 @@ package jp.freeapps.intellij.plugin.phparray.converter
 
 import com.intellij.testFramework.fixtures.LightJavaCodeInsightFixtureTestCase
 import com.jetbrains.php.lang.psi.PhpFile
+import jp.freeapps.intellij.plugin.phparray.exception.ConvertException
+import org.junit.Ignore
+import org.junit.Rule
 import org.junit.Test
+import org.junit.experimental.runners.Enclosed
+import org.junit.rules.ExpectedException
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import org.junit.runners.Parameterized.Parameter
 import org.junit.runners.Parameterized.Parameters
 import java.io.File
 
-@RunWith(Parameterized::class)
-internal class PhpArrayConverterTest : LightJavaCodeInsightFixtureTestCase() {
-    @Parameter
-    lateinit var title: String
+@RunWith(Enclosed::class)
+internal class PhpArrayConverterTest {
+    @RunWith(Parameterized::class)
+    internal class ToJsonTest : LightJavaCodeInsightFixtureTestCase() {
+        @Parameter
+        lateinit var title: String
 
-    @Parameter(1)
-    lateinit var argument: String
+        @Parameter(1)
+        lateinit var argument: String
 
-    @Parameter(2)
-    lateinit var expected: String
+        @Parameter(2)
+        lateinit var expected: String
 
-    @Test
-    fun test() {
-        val psiFile = myFixture.configureByText("target.php", argument)
-        val phpArrayConverter = PhpArrayConverter(psiFile as PhpFile)
+        @Test
+        fun test() {
+            val psiFile = myFixture.configureByText("target.php", argument)
+            val phpArrayConverter = PhpArrayConverter(psiFile as PhpFile)
 
-        // when:
-        val actual = phpArrayConverter.toJson()
+            // when:
+            val actual = phpArrayConverter.toJson()
 
-        // then:
-        assertEquals(expected, actual)
+            // then:
+            assertEquals(expected, actual)
+        }
+
+        companion object {
+            @JvmStatic
+            @Parameters(name = "{0}")
+            fun dataProvider(): Iterable<Any> {
+                return listOf(
+                    createPattern("keepFormat", "useArray,useSingleQuote"),
+                    createPattern("keepFormat", "useBraket,useSingleQuote"),
+                    createPattern("keepFormat", "useArray,useDoubleQuote"),
+                    createPattern("keepFormat", "useBraket,useDoubleQuote"),
+                    createPattern("syntax", "useArray,useSingleQuote"),
+                    createPattern("syntax", "useBraket,useSingleQuote"),
+                    createPattern("syntax", "useArray,useDoubleQuote"),
+                    createPattern("syntax", "useBraket,useDoubleQuote"),
+                    createPattern("syntax", "all"),
+                    createPattern("escapeSequence", "useArray,useSingleQuote"),
+                    createPattern("escapeSequence", "useBraket,useSingleQuote"),
+                    createPattern("escapeSequence", "useArray,useDoubleQuote"),
+                    createPattern("escapeSequence", "useBraket,useDoubleQuote"),
+                    createPattern("lastElementComma", "remove"),
+                    createPattern("keyPattern", "all"),
+                )
+            }
+
+            private fun createPattern(directory: String, pattern: String): Array<Any> {
+                return arrayOf(
+                    "$directory($pattern)",
+                    loadResource(directory, pattern, false),
+                    loadResource(directory, pattern, true),
+                )
+            }
+        }
     }
 
+    @RunWith(Parameterized::class)
+    internal class CheckValidTypeTest : LightJavaCodeInsightFixtureTestCase() {
+        @Parameter
+        lateinit var title: String
+
+        @Parameter(1)
+        lateinit var argument: String
+
+        @Parameter(2)
+        lateinit var expected: String
+
+        @get:Rule
+        val thrown: ExpectedException? = ExpectedException.none()
+
+        @Test
+        fun test() {
+            // setup:
+            val psiFile = myFixture.configureByText("target.php", argument)
+            val phpArrayConverter = PhpArrayConverter(psiFile as PhpFile)
+
+            // expect:
+            thrown!!.expect(ConvertException::class.java)
+            thrown!!.expectMessage(expected)
+            phpArrayConverter.toJson()
+        }
+
+        companion object {
+            @JvmStatic
+            @Parameters(name = "{0}")
+            fun dataProvider(): Iterable<Any> {
+                return listOf(
+                    *createPatterns("syntaxError", 3),
+                    *createPatterns("literalTypeError", 2),
+                )
+            }
+
+            private const val directory = "invalidType"
+            private fun createPatterns(pattern: String, numberOfPatterns: Int): Array<Any> {
+                return Array(numberOfPatterns) {
+                    val number = it + 1
+                    arrayOf(
+                        "$directory($pattern#$number)",
+                        loadResource("$directory/$pattern", "#$number", false),
+                        loadResource("$directory/$pattern", "#$number", true),
+                    )
+                }
+            }
+        }
+    }
+
+    @Ignore
     companion object {
-        @JvmStatic
-        @Parameters(name = "{0}")
-        fun dataProvider(): Iterable<Any> {
-            return listOf(
-                createPattern("keepFormat", "useArray,useSingleQuote"),
-                createPattern("keepFormat", "useBraket,useSingleQuote"),
-                createPattern("keepFormat", "useArray,useDoubleQuote"),
-                createPattern("keepFormat", "useBraket,useDoubleQuote"),
-                createPattern("syntax", "useArray,useSingleQuote"),
-                createPattern("syntax", "useBraket,useSingleQuote"),
-                createPattern("syntax", "useArray,useDoubleQuote"),
-                createPattern("syntax", "useBraket,useDoubleQuote"),
-                createPattern("syntax", "all"),
-                createPattern("escapeSequence", "useArray,useSingleQuote"),
-                createPattern("escapeSequence", "useBraket,useSingleQuote"),
-                createPattern("escapeSequence", "useArray,useDoubleQuote"),
-                createPattern("escapeSequence", "useBraket,useDoubleQuote"),
-                createPattern("lastElementComma", "remove"),
-                createPattern("keyPattern", "all"),
-            )
-        }
-
-        private fun createPattern(directory: String, pattern: String): Array<Any> {
-            return arrayOf(
-                "$directory($pattern)",
-                loadResource(directory, pattern, false),
-                loadResource(directory, pattern, true),
-            )
-        }
-
         private const val root = "src/test/resources/phpArrayToJson"
         private fun loadResource(directory: String, pattern: String, isExpected: Boolean): String {
             val name = if (isExpected) "expected" else "argument"
